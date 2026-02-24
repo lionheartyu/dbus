b79f9c0f50cca1281b7a24d776f939e2f92a0971
dongshengyuan <dongshengyuan@uniontech.com>
2026-02-24 14:11:52 +0800

    add-log-12

--- dbus/dbus-sysdeps-unix.c
+++ dbus/dbus-sysdeps-unix.c
@@ -2711,7 +2711,10 @@ fill_user_info (DBusUserInfo       *info,
             result = getpwuid_r(uid, &p_str, buf, buflen, &p);
             _dbus_warn("fill_user_info: called getpwuid_r, p=%p, p_str=%p\n", (void*)p, (void*)&p_str);
         } else {
-            sleep(5);
+          if (username_c && strcmp(username_c, "systemd-resolve") == 0) {
+              _dbus_warn("fill_user_info: sleep 5 seconds for systemd-resolve\n");
+              sleep(5);
+           }
             result = getpwnam_r(username_c, &p_str, buf, buflen, &p);
             _dbus_warn("fill_user_info: called getpwnam_r, p=%p, p_str=%p\n", (void*)p, (void*)&p_str);
         }
